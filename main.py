d5b0bb77b2bc4f07313e36ccb74d5f94156a8548
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-13 17:52:18 +0530

    Alarm Exception

--- main.py
+++ main.py
@@ -34,6 +34,19 @@ Boys5 = Boys(26,400,1)
 
 obj_config = Config()
 
-def moveJP():
-    def alarmhandler(signum,frame):
+def user_input(timeout=0.15):
+		''' input method '''
+		signal.signal(signal.SIGALRM, alarmhandler)
+		signal.setitimer(signal.ITIMER_REAL, timeout)
+		
+		try:
+			text = getChar()()
+			signal.alarm(0)
+			return text
+		except AlarmException:
+			pass
+		signal.signal(signal.SIGALRM, signal.SIG_IGN)
+		return ''
+
+        
     
