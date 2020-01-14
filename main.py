ed9c27d695028740c8a2d180e60342a9f49817b0
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-14 11:00:31 +0530

    Colorama

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from board import Board
+import os
 from boys import Boys
 from jetpacker import Jet_Packer
 from Scenery import Scenery
@@ -6,7 +7,7 @@ from getch import *
 from config import Config
 import signal
 from alarmexception import AlarmException
-from coloroma import init,Fore
+from colorama import init,Fore
 init()
 objB=Board(30,500)
 
@@ -47,6 +48,100 @@ def user_input(timeout=0.15):
 			pass
 		signal.signal(signal.SIGALRM, signal.SIG_IGN)
 		return ''
+# char = user_input()
 
-        
-    
+# 	if char == 'd':
+# 		obj_config.coins_right(objB.matrix, obj_mario)
+# 		can_he=obj_mario.check_not_collision_right(objB.matrix)
+
+# 		if(objB.matrix[obj_mario.yc-5][obj_mario.xc + 1] == 'B'):
+# 			objB.matrix[obj_mario.yc-5][obj_mario.xc + 1] = " "
+
+		
+# 		if can_he == 1:
+# 			obj_mario.disappear_mario(objB)
+# 			obj_mario.xcoo+=1
+# 			obj_mario.direction = 1
+# 			obj_mario.reappear_mario(objB)
+
+# 		elif can_he == 2:
+# 			obj_mario.life -= 1
+# 			os.system('afplay ./music/mario_dies.wav&')
+# 			objB.spawn_mario(obj_mario)
+# 			obj_mario.did_he_die = 0
+
+
+# 		else:
+# 			os.system('afplay ./music/bump.wav&')
+
+# 	if char == 'a':
+		
+# 		obj_config.coins_left(objB.matrix, obj_mario)
+# 		can_he=obj_mario.check_not_collision_left(objB.matrix)
+
+# 		if(objB.matrix[obj_mario.ycoo-5][obj_mario.xcoo + 1] == 'B'):
+# 			objB.matrix[obj_mario.ycoo-5][obj_mario.xcoo + 1] = " "
+		
+# 		if can_he == 1:
+# 			obj_mario.disappear_mario(objB)
+# 			obj_mario.xcoo -= 1
+# 			obj_mario.direction = -1
+# 			obj_mario.reappear_mario(objB)
+
+# 		elif can_he == 2:
+# 			obj_mario.life -= 1
+# 			os.system('afplay ./music/mario_dies.wav&')
+# 			objB.spawn_mario(obj_mario)
+# 			obj_mario.did_he_die = 0
+
+# 		else:
+# 			os.system('afplay ./music/bump.wav&')
+				
+# 	if char == 'q':
+# 		os.system("killall afplay")
+# 		os.system('afplay ./music/game_over.wav&')
+# 		quit()
+	
+# 	if char == 'w':
+# 		if(obj_board.matrix[obj_mario.ycoo + 3][obj_mario.xcoo] == "-"): #standing on surface
+
+# 			prev_ycoo=obj_mario.ycoo
+			
+# 			while(obj_mario.ycoo != prev_ycoo-8 and # 8 units; checking if there's anything above
+# 				obj_board.matrix[obj_mario.ycoo-1][obj_mario.xcoo+2] == " " and
+# 				obj_board.matrix[obj_mario.ycoo-1][obj_mario.xcoo+1] == " " and
+# 				obj_board.matrix[obj_mario.ycoo-1][obj_mario.xcoo] == " "): 
+
+# 				obj_mario.disappear_mario(obj_board)
+# 				obj_mario.ycoo -= 1
+
+# 				obj_mario.reappear_mario(obj_board)
+
+# 			os.system('afplay ./music/jump.wav&')
+# 			obj_config.check_brick_collision(obj_scenery, obj_board, obj_mario)
+
+# 	if char == 's':
+# 		obj_board.matrix[obj_mario.ycoo-5][obj_mario.xcoo + 1] = 'B'
+# 		os.system('afplay ./music/bullet.wav&')
+
+# 		bosskill = obj_bossenemy.check_boss_kill(obj_board, obj_mario)
+# 		# if(bosskill is False):
+# 		# 	obj_board.matrix[obj_mario.ycoo-5][obj_mario.xcoo] = " "
+# 		# else:
+# 		if bosskill is True:
+# 			if(obj_bossenemy.boss_life == 1):
+# 				obj_bossenemy.boss_kill = True
+# 				obj_scenery.remove_barrier(obj_board.matrix)
+# 			else:
+# 				obj_bossenemy.boss_life -= 1
+
+# x=time.time()
+# y=x #copy
+# z=x #copy
+
+
+# os.system('afplay ./music/theme.mp3&')
+while True:
+    os.system('clear')
+    end
+              
