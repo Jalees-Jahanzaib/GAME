a7eeb5c32529b331988220776b0944deb9b8ed5e
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-15 08:32:43 +0530

    2

--- Scenery.py
+++ Scenery.py
@@ -52,22 +52,6 @@ class Scenery:
 			c=f +random.randint(0,2)
 			d += 37 + random.randint(10,50) 
 
-	def create_tunnels(self,grid,c,d):
-		with open("./background/tunnel.txt") as obj:
-			for line in obj:
-				self.__tunnel.append(line.strip('\n'))
-
-		while(d<360):
-			e=d
-			f=c
-			for i in range(5):
-				for j in range(13):
-					grid[c][d] = self.__tunnel[i][j]
-					d+=1
-				d=e
-				c+=1
-			c=f
-			d+=70 
 
 	def create_bricks(self,grid,c,d):
 		with open("./background/brick.txt") as obj:
@@ -89,55 +73,8 @@ class Scenery:
 			c=f
 			d+=70 
 
-	def create_mountain(self,grid,c,d):
-		with open("./background/mountain.txt") as obj:
-			for line in obj:
-				self.__mountain.append(line.strip('\n'))
-		e=d
-		for i in range(10):
-			for j in range(68):
-				grid[c][d] = self.__mountain[i][j]
-				d+=1
-			d=e
-			c+=1
 
-	def create_holes(self, obj_board):
-		for i in range (40,45):
-			obj_board.matrix[28][i] = " "
-		for i in range (290,295):
-			obj_board.matrix[28][i] = " "
-		for i in range (323,328):
-			obj_board.matrix[28][i] = " "
-		for i in range (360,365):
-			obj_board.matrix[28][i] = " "
-
-
-
-	def create_springs(self, obj_board):
-		
-		obj_board.matrix[27][55] = self.spring 
-		obj_board.matrix[27][195] = self.spring 
-
-	def create_coins_platforms(self, obj_board):
-		for i in range(115,130):
-			obj_board.matrix[23][i] = "-"
-		for i in range(115,130):
-			obj_board.matrix[27][i] = "$"
-		
-		for i in range (320,334):
-			obj_board.matrix[23][i] = "-"
-		for i in range(320,334):
-			obj_board.matrix[27][i] = "$"
-
-
-	def put_barrier(self,grid):
-		for i in range(30):
-			grid[i][492] = '|'
-
-	def remove_barrier(self,grid):
-		for i in range(2,28):
-			grid[i][492] = " "
-
+ 
 
 
 
--- main.py
+++ main.py
@@ -19,13 +19,6 @@ obj_scenery=Scenery()
 obj_scenery.create_ground(objB.matrix)
 obj_scenery.create_sky(objB.matrix)
 obj_scenery.create_clouds(objB.matrix, 2, 11)
-obj_scenery.create_tunnels(objB.matrix, 23, 19)
-obj_scenery.create_bricks(objB.matrix, 19, 57)
-obj_scenery.create_mountain(objB.matrix, 3, 294)
-obj_scenery.put_barrier(objB.matrix)
-obj_scenery.create_springs(objB)
-obj_scenery.create_holes(objB)
-obj_scenery.create_coins_platforms(objB)
 
 obj_config = Config()
 
