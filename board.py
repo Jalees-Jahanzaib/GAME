f6189f9a8b6a205af59b5fae9b5fa0c0b787b314
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-16 12:17:03 +0530

    Board refactored

--- board.py
+++ board.py
@@ -1,40 +1,39 @@
 import os
 class Board:
-	'''This class creates the grid for the game, and displays it'''
 	
-	def __init__(self, rows, columns): # initialize values
+	def __init__(self, rows, columns): 
 		self.rows = rows
 		self.columns = columns
 		self.matrix = []
 
-	def create_board(self): # creates the entire grid
+	def create_board(self):
 
-		for i in range (self.rows): #rows                              
+		for i in range (self.rows):                             
 			self.new = []                 
-			for j in range (self.columns): #columns  
+			for j in range (self.columns):   
 				self.new.append(" ")      
 			self.matrix.append(self.new)
 
-	def theyllprintit(self, a): # A reference to the Three Days of the Condor :P
-		if(a==0): #the grid doesn't move in the beginning
+	def theyllprintit(self, a):
+		if(a==0):
 			for i in range(self.rows):
-				for j in range(a,a+110): # 110 columns at a time
+				for j in range(a,a+110):
 					print(self.matrix[i][j],end='')
 				print()
 		
-		elif(a == 444): # the grid stops moving from columns 444 to 499
+		elif(a == 444): 
 			for i in range(self.rows):
-				for j in range(444-55, 444+55): # 110 columns at a time
+				for j in range(444-55, 444+55): 
 					print(self.matrix[i][j],end='')
 				print()
 
-		else:  # Now it starts moving!
+		else:  
 			for i in range(self.rows):
-				for j in range(a-55, a+55): # 110 columns at a time
+				for j in range(a-55, a+55): 
 					print(self.matrix[i][j],end='')
 				print()
 
-	def spawn_mario(self, obj_mario): # Spawns Mario after he dies
+	def spawn_mario(self, obj_mario): 
 
 		obj_mario.disappear_mario(self)
 						
@@ -43,10 +42,6 @@ class Board:
 
 		obj_mario.reappear_mario(self)
 
-	def jump_higher(self, obj_mario):
 
-		obj_mario.disappear_mario(self)
-		obj_mario.ycoo -= 16
-		obj_mario.reappear_mario(self)
 
 		
