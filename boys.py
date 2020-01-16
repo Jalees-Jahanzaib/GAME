5887476587bfa8ad96c75e7fb8ee987287794e35
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-16 07:17:45 +0530

    Boys Finished

--- boys.py
+++ boys.py
@@ -7,24 +7,19 @@ class Character_Structure:
         self.yc=yc
         self.dirction=direction
 class Boys(Character_Structure):
-    def __init__(self,xc,yc,direction):
-        Character_Structure.__init__(self,xc,yc,direction)
-        self.__shape=[["*","*"],["*","*"]]
-        self.allowed_collision=[" "]
-        self.hit_JP=["^"]
-        self.killed=0
-    def starting_position(self, grid):
-        for i in range(self.yc,self.yc+2):
-            for j in range(self.xc,self.xc+2):
-                grid[i][j] = self.__shape[i-self.yc][j-self.xc]
-    def disappear_enemy(self, grid):
-        for i in range(self.yc, self.yc+2, 1):
-            for j in range(self.xc, self.xc+2, 1):
-                grid[i][j] = " "
-        def reappear_enemy(self, grid):
-            for i in range(self.yc, self.yc + 2, 1):
-                for j in range(self.xc, self.xc + 2, 1):
-                    grid[i][j] = self.__shape[i - self.yc][j - self.xc]
+ 	def __init__(self, ycoo, xcoo, dire):
+		Main_person.__init__(self, ycoo, xcoo, dire)
+		self.__shape = [ ["+", "+"], ["@", "@"] ]
+		self.allowed_collision = [ " " ]
+		self.hit_with_mario = [ "^" ]
+		self.killed = 0
+
+	def starting_position1(self, grid):
+		for i in range(self.ycoo,self.ycoo+4):
+				grid[i][self.xcoo] = '+'
+	def starting_position2(self, grid):
+		for i in range(self.xcoo,self.xcoo+6):
+				grid[self.ycoo][i] = '+'
 
 
 
