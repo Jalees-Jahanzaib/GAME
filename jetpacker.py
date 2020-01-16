62f225c0d3ed1d603aef2c645b42c738c4bfb0b2
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2020-01-16 07:22:58 +0530

    Jetpacker Finished

--- jetpacker.py
+++ jetpacker.py
@@ -1,37 +1,65 @@
-from boys import Character_Structure
+from enemy import Main_person
+class JetPacker(Main_person):
+	
+	
+	def __init__(self, yc, xc, dire): 
+		
+		Main_person.__init__(self, yc, xc, dire)
+		self.__shape1 = [ [" ", 'O', " "], ["[", "|", "<"], [" ", "^", " "] ]
+		self.__shape2 = [ [" ", 'O', " "], [">", "|", "]"], [" ", "^", " "] ]
+		self.life = 5
+		self.allowed_collision = [ " ", "$" ]
+		self.coins = 0
+		self.did_he_die = 0
 
-class Jet_Packer(Character_Structure):
-    def __init__(self,yc,xc,direction):
-        Character_Structure.__init__(self,yc,xc,direction)
-        self.__shape1 = [ [" ", 'O', " "], ["[", "|", "<"], [" ", "^", " "] ]
-        self.__shape2 = [ [" ", 'O', " "], [">", "|", "]"], [" ", "^", " "] ]
-        self.__shape3= [ ["| ", 'O', " |"], ["{", "|", "}"], [" ", "^", " "] ]
-        self.life=3
-        self.allowed_collision=[" ","$"]
-        self.coins=0
-        self.death=0
-        self.power_up=False
-    def starting_p(self,grid):
-        for i in range(25,28,1):
-            for j in range(0,3,1):
-                grid[i][j]=self.__shape1[i-25][j]
-    def check_not_collision_right(self, grid):
-        if (grid[self.yc][self.xc+3] in self.allowed_collision 
+	def starting_position(self, grid):
+		
+		for i in range(25,28,1):
+			for j in range(0, 3, 1):
+				grid[i][j] = self.__shape1[i-25][j]
+
+	def check_not_collision_right(self, grid):
+		
+		if (grid[self.yc][self.xc+3] in self.allowed_collision 
 			and grid[self.yc+1][self.xc+3] in self.allowed_collision
 			and grid[self.yc+2][self.xc+3] in self.allowed_collision):
-            return 1
-        elif (grid[self.yc + 1][self.xc + 3] == "*" 
-			or grid[self.yc+2][self.xc+3] == "*"):
-            return 2
-        else:
-            return 3
-  
-    def jetpackerDisappear(self, obj_board):
-        for i in range(self.yc, self.yc+3):
-            for j in range(self.xc, self.xc+3):
-                obj_board.matrix[i][j] = " "
-    def reappear(self, obj_board):
-        for i in range(self.yc, self.yc+3, 1):
-            for j in range(self.xc, self.xc+3, 1):
-                    obj_board.matrix[i][j] = self.__shape1[i-self.yc][j-self.xc]
-                
+
+			return 1
+		
+		
+		elif (grid[self.yc + 1][self.xc + 3] == "+" 
+			or grid[self.yc+2][self.xc+3] == "+"):
+			return 2
+		
+		else:
+			return 3
+		
+	def check_not_collision_left(self, grid):
+
+		if (grid[self.yc][self.xc-1] in self.allowed_collision 
+			and grid[self.yc+1][self.xc-1] in self.allowed_collision
+			and grid[self.yc+2][self.xc-1] in self.allowed_collision
+			and self.xc-1 != -1): # last condition for not going out of the board at -1th column
+
+			return 1
+
+		elif (grid[self.yc + 1][self.xc - 1] == "+" 
+			or grid[self.yc+2][self.xc - 1] == "+"):
+			
+			return 2
+
+		else:
+			return 3
+	
+	def jetpackerDisappear(self, obj_board):
+		for i in range(self.yc, self.yc+3):
+			for j in range(self.xc, self.xc+3):
+				obj_board.matrix[i][j] = " "
+
+	def reappear(self, obj_board):
+		for i in range(self.yc, self.yc+3, 1):
+			for j in range(self.xc, self.xc+3, 1):
+				if self.direction == 1:
+						obj_board.matrix[i][j] = self.__shape1[i-self.yc][j-self.xc]
+				else:
+					obj_board.matrix[i][j] = self.__shape2[i-self.yc][j-self.xc]
